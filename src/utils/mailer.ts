d55571da7b68a829e20677c824457d100e26934c
Soe Phyu <soephyuphyuhtun99@gmail.com>
2023-09-04 10:17:47 +0630

    VTP-2 update keys

--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -1,25 +1,21 @@
 import emailjs from "@emailjs/browser";
 const sendMail = (data) => {
+	const mailServiceKey = "service_cekrbgb";
+	const templateKey = "template_6mpgakk";
+	const publicKey = "L36M0C8GcRtJFUMRO";
 	const templatePrams = {
 		name: data.name,
 		email: data.email,
 		phone: data.phone,
 		description: data.description,
 	};
-	emailjs
-		.send(
-			"service_cekrbgb",
-			"template_6mpgakk",
-			templatePrams,
-			"L36M0C8GcRtJFUMRO"
-		)
-		.then(
-			(response) => {
-				console.log("SUCCESS!", response.status, response.text);
-			},
-			(err) => {
-				console.log("FAILED...", err);
-			}
-		);
+	emailjs.send(mailServiceKey, templateKey, templatePrams, publicKey).then(
+		(response) => {
+			console.log("SUCCESS!", response.status, response.text);
+		},
+		(err) => {
+			console.log("FAILED...", err);
+		}
+	);
 };
 export default sendMail;
